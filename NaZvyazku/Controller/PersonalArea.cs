8f790a891cb78f568f8349fcf7e1d52698a774e8
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2022-11-06 21:58:18 +0200

    Add Controller

---
+++ NaZvyazku/Controller/PersonalArea.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace NaZvyazku.Controller
+{
+    public class PersonalArea
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public Image Avatar { get; set; }
+        public int TariffID { get; set; }
+        public int SubscriptionID { get; set; }
+        public int Cash { get; set; }
+        public int Gigabyte { get; set; }
+        public DateTime DateReg { get; set; }
+
+        public PersonalArea(int userId, string fullName, Image avatar, int tariffID, int subscriptionID, int cash, int gigabyte, DateTime dateReg)
+        {
+            UserId = userId;
+            FullName = fullName;
+            Avatar = avatar;
+            TariffID = tariffID;
+            SubscriptionID = subscriptionID;
+            Cash = cash;
+            Gigabyte = gigabyte;
+            DateReg = dateReg;
+        }
+    }
+}
---
+++ NaZvyazku/Controller/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaZvyazku.Controller
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateReg { get; set; }
+        public int RoleID { get; set; }
+        public int AuthorizationID { get; set; }    
+
+        public User(int UserId, string Fullname, DateTime datereg, int RoleId, int AuthorizationId)
+        {
+            UserID = UserId;
+            FullName = Fullname;
+            DateReg = datereg;
+            RoleID = RoleId;
+            AuthorizationID = AuthorizationId;
+        }
+    }
+}
