4614f5130ec918350bc47b9e70bed51d5edfdcac
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2022-11-15 18:16:08 +0200

    Добавил модели

---
+++ NaZvyazku/ZvyazokDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NaZvyazku.Controller;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaZvyazku
+{
+    public class ZvyazokDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<PersonalArea> personalAreas { get; set; }
+        public DbSet<Authorization> authorizations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(GetConnectionStrings());
+        }
+
+        public static string GetConnectionStrings()
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings stringSettings in settings)
+                {
+                    return stringSettings.ConnectionString;
+                }
+            }
+            return "";
+        }
+    }
+}
